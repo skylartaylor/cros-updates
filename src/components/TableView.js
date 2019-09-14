e0600c2f76cb9ca36e6d051dbeed096044a523b0
Skylar Taylor-Barrick <hi@skylar.cc>
2019-09-14 12:49:04 -0700

    remove dummy table data

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -18,18 +18,6 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function TableView(props) {
   const classes = useStyles();
   const data = props.data
