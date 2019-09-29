946d5c90e59ddd3f289506b33e69a70e1416787e
Skylar Taylor-Barrick <hi@skylar.cc>
2019-09-29 16:59:22 -0700

    Remove HTML from Codename, add to Versions

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,23 +1,26 @@
 var tabletojson = require("tabletojson")
 const fs = require("fs")
 const { Parser } = require("json2csv")
+const stripHtml = require("string-strip-html")
 
 var url = "https://cros-updates-serving.appspot.com/"
 
 tabletojson.convertUrl(url, { stripHtmlFromCells: false }, function(
   tablesAsJson
 ) {
-  fs.writeFile(
-    "./src/data/cros-updates.json",
-    JSON.stringify(tablesAsJson[0], null, 4),
-    err => {
-      // throws an error, you could also catch it here
-      if (err) throw err
-    }
-  )
+  var data = tablesAsJson[0].map(item => {
+    item.Codename = stripHtml(item.Codename)
+    return item
+  })
+
+  var json = JSON.stringify(data, null, 4)
+
+  fs.writeFile("./src/data/cros-updates.json", json, err => {
+    if (err) throw err
+  })
 
   const json2csvParser = new Parser()
-  const csv = json2csvParser.parse(tablesAsJson[0])
+  const csv = json2csvParser.parse(data)
 
   fs.writeFile("./src/data/cros-updates.csv", csv, err => {
     if (err) throw err
